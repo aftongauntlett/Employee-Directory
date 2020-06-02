ab0c01cb6e043d8e081a74aed487b3b53538ecba
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-02 18:56:37 -0400

    adding components and setting up layout

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class EmployeeList extends Component {
+  render() {
+    return <div>Employee List</div>;
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../css/Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>Employee Directory</h1>
+        <div>
+          Click on the carats to filter by heading or uses the search box to
+          narrow your results.
+        </div>
+      </div>
+    );
+  }
+}
